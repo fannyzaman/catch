c42124c97101e5b2d3f33ee94dee89c29e88868a
fannyzaman <47830861+fannyzaman@users.noreply.github.com>
2023-04-22 11:03:58 +0200

    Update vangen.js

--- interacieve_quiz/js/vangen.js
+++ interacieve_quiz/js/vangen.js
@@ -1,7 +1,7 @@
 let score = 0;
 let lives = 3;
 let boyX = 6;
-let boyY = 15;
+let boyY = 13;
 let pizzaX = 6;
 let pizzaY = 0;
 let pizzavangerX = 6; // initialize the position of the pizzavanger
@@ -25,7 +25,7 @@ function startGame() {
   score = 0;
   lives = 3;
   boyX = 6;
-  boyY = 15;
+  boyY = 13;
   pizzaX = 6;
   pizzaY = 0;
   gameTimer = window.setInterval(movePizza, 400);
